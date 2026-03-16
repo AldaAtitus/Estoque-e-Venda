14b01a936a46a4d6dfd4d11923fcb63be451c825
henri <henrique17122005@gmail.com>
2026-03-16 20:05:55 -0300

    SistemaEstoque Update em base de ódio.

--- main.py
+++ main.py
@@ -9,3 +9,32 @@ from servicos.estoque_servico import EstoqueServico
 from servicos.venda_servico import VendaServico
 from estruturas.pilha import Pilha
 from utils.validar import let_int, ler_sim_nao
+
+class SistemaEstoque:
+    def __init__(self):
+        # Inicializa o sistema carregando os dados.
+        try:
+            # Carregamento automático.
+            self.clientes = GerenciadorArquivos.carregar_clientes()
+            self.produtos = GerenciadorArquivos.carregar_produtos()
+            self.vendas = GerenciadorArquivos.carregar_vendas()
+            # Pilha para desfazer operações.
+            self.pilha_operacoes = Pilha()
+            # Inicializar servições.
+            self.cliente_servico = ClienteServico(self.clientes, self.persistir_dados)
+            self.estoque_servico = EstoqueServico(self.produtos, self.persistir_dados, self.vendas, self.empilhar_operacao)
+            self.venda_servico = VendaServico(self.vendas, self.cliente_servico, self.estoque_servico, self.persistir_dados, self.empilhar_operacao)
+
+            print("\n" + "=" * 50)
+            print("SISTEMA INICIADO COM SUCESSO!")
+            print(f"Clientes: {len(self.clientes.listar()) if not self.clientes.vazia() else 0}")
+            print(f"Produtos: {len(self.produtos.listar()) if not self.produtos.vazia() else 0}")
+            print(f"Vendas: {self.vendas.tamanho()}")
+            print("\n" + "=" * 50)
+
+        except Exception as e:
+            print("\n" + "=" * 50)
+            print("EROO NA INICIALIZAÇÃO!")
+            print(f"Erro: {e}")
+            print("\n" + "=" * 50)
+            raise e
