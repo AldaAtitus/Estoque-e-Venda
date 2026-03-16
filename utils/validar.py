868b17e51cc399d89da738f9583e65f7d04a0251
henri <henrique17122005@gmail.com>
2026-03-16 19:30:25 -0300

    Float update.

--- utils/validar.py
+++ utils/validar.py
@@ -1,12 +1,25 @@
 def ler_int(mensagem, permitir_cancelar=True):
-    # Lê um número inteiro com tratamento de erros
+    # Lê um número inteiro com tratamento de erros.
     while True:
         try:
             valor = input(mensagem).strip()
-            # Verificar cancelamento ("c ou C")
+            # Verificar cancelamento ("c ou C").
             if permitir_cancelar and valor.lower() == "c":
                 print("Operação cancelada. Voltando ao menu principal.")
                 return None
             return int(valor)
+        except ValueError:
+            print("Entrada inválida. Por favor, digite um número válido ou 'C' para cancelar.")
+
+def ler_float(mensagem, permitir_cancelar=True):
+    # Lê um número de ponto flutuante com tratamento de erros.
+    while True:
+        try:
+            valor = input(mensagem).strip()
+            # Verificar cancelamento ("c ou C").
+            if permitir_cancelar and valor.lower() == "c":
+                print("Operação cancelada. Voltando ao menu principal.")
+                return None
+            return float(valor)
         except ValueError:
             print("Entrada inválida. Por favor, digite um número válido ou 'C' para cancelar.")
